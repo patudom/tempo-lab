9c6da8713766c0a4d65b0c37fdd449e6b3d7153e
Carifio24 <carifio24@gmail.com>
2025-07-30 23:11:34 -0400

    Update list of colors.

--- src/TempoLite.vue
+++ src/TempoLite.vue
@@ -1537,11 +1537,13 @@ const selectionOptions = computed<SelectionOption[]>(() => ([null] as SelectionO
 let selectionCount = 0;
 const samplesGraph = ref(false);
 const haveSamples = computed(() => selections.value.some(s => s.samples));
+
+// I stole this from here: https://sashamaps.net/docs/resources/20-colors/
 const COLORS = [
-  "#ff0000",
-  "#00ff00",
-  "#0000ff",
-  "#ffff00",
+  '#e6194b', '#3cb44b', '#ffe119', '#4363d8', '#f58231', '#911eb4', '#46f0f0',
+  '#f032e6', '#bcf60c', '#fabebe', '#008080', '#e6beff', '#9a6324', '#fffac8',
+  '#800000', '#aaffc3', '#808000', '#ffd8b1', '#000075', '#808080', '#ffffff',
+  '#000000'
 ];
 
 // implement rectangle and point
