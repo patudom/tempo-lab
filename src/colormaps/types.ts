e40e5e647e3f14664b7a95fb29f6da2ec38405c9
John Arban Lewis <johnarban@users.noreply.github.com>
2025-08-19 12:20:10 -0400

    fix lint error

--- src/colormaps/types.ts
+++ src/colormaps/types.ts
@@ -1,4 +1,4 @@
-export type ColorMap = {r: number[], g: number[], b: number[]}
+export type ColorMap = {r: number[], g: number[], b: number[]};
 
 export interface ColorMaps {
     [key: string]: ColorMap;
