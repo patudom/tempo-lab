6fced2f871a078323315d4f26a79d4891da1508e
Carifio24 <carifio24@gmail.com>
2025-09-25 13:06:46 -0400

    WIP: More work on control item.

--- src/components/MaplibreLayerControlItem.vue
+++ src/components/MaplibreLayerControlItem.vue
@@ -10,8 +10,7 @@
         type="checkbox"
         :id="`mlc-${layer.id}-visibility-checkbox`"
         class="mlc-layer-item-checkbox"
-      >
-      </input>
+      />
       <label
         class="mlc-layer-item-label"
         :for="`mlc-${layer.id}-visibility-checkbox`"
@@ -19,9 +18,39 @@
         {{ layer.id }}
       </label>
     </div>
+    <div
+      class="mlc-layer-item-opacity-label-container"
+    >
+      <input
+        :id="`mlc-${layer.id}-opacity-slider`"
+        class="mlc-layer-opacity-slider"
+        type="range"
+        min="0"
+        max="1"
+        step="0.01"
+        title="Adjust layer opacity"
+      />
+      <label
+        :for="`mlc-${layer.id}-opacity-slider`"
+      >
+        Transparency: 
+      </label>
+    </div>
   </div>
 </template>
 
 <script setup lang="ts">
+import { ref } from "vue";
+import { LayerSpecification } from "maplibre-gl";
 
+interface Props {
+  layer: LayerSpecification;
+  initialOpacity?: number;
+}
+
+const props = withDefaults(defineProps<Props>(), {
+  initialOpacity: 1,
+});
+
+const opacity = ref(props.initialOpacity);
 </script>
---
+++ src/maplibre_controls.ts
@@ -0,0 +1,58 @@
+import type { Map } from "maplibre-gl";
+//
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+function isNumeric(value: any): boolean {
+  return !isNaN(value - parseFloat(value));
+}
+
+export function setLayerOpacity(map: Map, layerId: string, opacity: number) {
+  const layer = map.getLayer(layerId)!;
+  if (!layer) {
+    console.warn('Layer not found:', layerId);
+    return;
+  }
+
+  if (layer.type !== 'symbol') {
+    const paintProp = `${layer.type}-opacity`;
+    map.setPaintProperty(layerId, paintProp, opacity);
+  }
+  if (layer.type === 'circle') {
+    if (map.getPaintProperty(layerId, 'circle-opacity') !== undefined) {
+      map.setPaintProperty(layerId, 'circle-stroke-opacity', opacity);
+    }
+  }  
+  if (layer.type === 'symbol') {
+    // icon-opacity
+    if (map.getPaintProperty(layerId, 'icon-opacity') !== undefined) {
+      map.setPaintProperty(layerId, 'icon-opacity', opacity);
+    }
+    // text-opacity
+    if (map.getPaintProperty(layerId, 'text-opacity') !== undefined) {
+      map.setPaintProperty(layerId, 'text-opacity', opacity);
+    }
+  }
+}
+
+export function getLayerOpacity(map: Map, layerId: string): number {
+  const layer = map.getLayer(layerId);
+  if (!layer) return 1;
+  
+  let op: unknown = 1;
+  
+  if (layer.type === 'symbol') {
+    op = map.getPaintProperty(layerId, 'icon-opacity');
+    if (isNaN(op as unknown as number)) {
+      op = map.getPaintProperty(layerId, 'text-opacity');
+    }
+  } else if (layer.type === 'circle') {
+    op = map.getPaintProperty(layerId, 'circle-opacity');
+    if (isNaN(op as unknown as number)) {
+      op = map.getPaintProperty(layerId, 'circle-stroke-opacity');
+    }
+  } else {
+    const paintProp = `${layer.type}-opacity`;
+    op = map.getPaintProperty(layerId, paintProp);
+  }
+  
+  return isNumeric(op) ? (op as unknown as number) : 1;
+} 
