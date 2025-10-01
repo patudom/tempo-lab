9c6a59ec8b6e26acda8d426bd90e64d533813568
John Arban Lewis <johnarban@users.noreply.github.com>
2025-10-01 16:47:37 -0400

    add seasons

--- src/date_time_range_selection/DateTimeRangeSelection.vue
+++ src/date_time_range_selection/DateTimeRangeSelection.vue
@@ -201,7 +201,8 @@
       <v-expand-transition>
         <div v-if="selectionType === 'monthrange'" class="month-selection-section">
           <div class="mb-4">
-            <label v-for="month in monthNames" :key="month" class="mr-3" style="text-wrap: nowrap;" for="dtrs-month-{{ month }}">
+            <div  v-for="month in monthNames" :key="month" class="d-inline-block">
+            <label class="mr-3" style="text-wrap: nowrap;" for="dtrs-month-{{ month }}"></label>
               <input 
                 id="dtrs-month-{{ month }}"
                 type="checkbox" 
@@ -209,11 +210,12 @@
                 v-model="selectedMonths" 
               />
               <span class="ml-1">{{ month }}</span>
-            </label>
+            </div>
           </div>
           
           <div class="mb-4">
-            <label v-for="year in possibleYears" :key="year" class="mr-3" style="text-wrap: nowrap;" for="dtrs-year-{{ year }}">
+            <div v-for="year in possibleYears" :key="year" class="d-inline-block">
+            <label  class="mr-3" style="text-wrap: nowrap;" for="dtrs-year-{{ year }}"></label>
               <input 
                 id="dtrs-year-{{ year }}"
                 type="checkbox" 
@@ -221,7 +223,43 @@
                 v-model="selectedYears" 
               />
               <span class="ml-1">{{ year }}</span>
-            </label>
+            </div>
+            
+          </div>
+          
+          <div class="mb-4 simple-patterns">
+            <!-- winter, spring, summer, fall -->
+            <v-btn 
+              size="x-small" 
+              variant="outlined" 
+              class="mr-2 mb-2"
+              @click="selectedMonths = [11,0,1]"
+            >Winter</v-btn>
+            <v-btn 
+              size="x-small" 
+              variant="outlined" 
+              class="mr-2 mb-2"
+              @click="selectedMonths = [2,3,4]"
+            >Spring</v-btn>
+            <v-btn 
+              size="x-small" 
+              variant="outlined" 
+              class="mr-2 mb-2"
+              @click="selectedMonths = [5,6,7]"
+            >Summer</v-btn>
+            <v-btn 
+              size="x-small" 
+              variant="outlined" 
+              class="mr-2 mb-2"
+              @click="selectedMonths = [8,9,10]"
+            >Fall</v-btn>
+            <!-- clear -->
+            <v-btn 
+              size="x-small" 
+              variant="outlined" 
+              class="mr-2 mb-2"
+              @click="selectedMonths = []"
+            >Clear Months</v-btn>
           </div>
         </div>
       </v-expand-transition>
