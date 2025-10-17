53b8b0cb1533c80d441b95716ce691c575cc7508
John Arban Lewis <johnarban@users.noreply.github.com>
2025-10-17 10:17:30 -0400

    move button into panel

--- src/components/DatasetControls.vue
+++ src/components/DatasetControls.vue
@@ -390,18 +390,18 @@
                   </template>
             </dataset-card>
           </div>
+          <div v-if="datasets.some(s => s.timeRange.type === 'folded')" class="pa-2 mb-2 explainer-text">
+            Note: You can only overlay datasets with the same molecule and fold type.
+          </div>
+          <v-btn color="#ffcc33" size="small" :block="false" @click="allDatasetSelection = !allDatasetSelection">
+            {{ allDatasetSelection ? 'Cancel Selection' : 'Select Datasets to Graph' }}
+          </v-btn>
         </template>
       </v-expansion-panel>
     </v-expansion-panels>
     </div>
 
     <div class="d-flex flex-wrap flex-row ma-2 align-center justify-center ga-1">
-      <div v-if="datasets.some(s => s.timeRange.type === 'folded')" class="pa-2 mb-2 explainer-text">
-        Note: You can only overlay datasets with the same molecule and fold type.
-      </div>
-      <v-btn color="#ffcc33" size="small" :block="false" @click="allDatasetSelection = !allDatasetSelection">
-        {{ allDatasetSelection ? 'Cancel Selection' : 'Select Datasets to Graph' }}
-      </v-btn>
     <v-btn v-if="no2GraphData.length > 0" @click="showNO2Graph = true">
       Show NO₂ Graph
     </v-btn>
