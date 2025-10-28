577ce8946f3400e38917a68292a73fbad786dcb6
Pat Udomprasert <pudompra@cfa.harvard.edu>
2025-10-28 13:46:04 -0400

    Switch order for intro text & delete button

--- src/components/DatasetControls.vue
+++ src/components/DatasetControls.vue
@@ -1,6 +1,10 @@
 <template>
   <div id="dataset-sections" :style="cssVars">
     <h2>Investigate Patterns with Time</h2>
+    <div id="add-region-time">
+      <p>
+        Select a Region, Time Range, and Molecule to create a dataset of your choice!
+      </p>
     <v-btn
       v-if="regions.length > 0 || timeRanges.length > 1"
       :color="accentColor2"
@@ -38,10 +42,6 @@
         </v-card-actions>
       </v-card>
     </v-dialog>
-    <div id="add-region-time">
-      <p>
-        Select a Region, Time Range, and Molecule to create a dataset of your choice!
-      </p>
       <v-expansion-panels
         v-model="openPanels"
         variant="accordion"
