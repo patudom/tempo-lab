2d006f0a5e88b55462fa786be9204207583b3be1
Carifio24 <carifio24@gmail.com>
2025-11-11 11:21:30 -0500

    Toggle on label press and add drop shadow.

--- src/components/IconCheckbox.vue
+++ src/components/IconCheckbox.vue
@@ -4,18 +4,23 @@
     :value="value"
     v-bind="props"
   >
+    <template #label>
+      <span @click="() => update()">
+        {{ label }}
+      </span>
+    </template>
     <template #input="{ model }">
       <!-- Using <component :is="..."/> didn't work with v-icon for some reason -->
       <font-awesome-icon
           v-if="(model.value ? onIcon : offIcon)?.startsWith('fa-')"
           :icon="model.value ? onIcon : offIcon"
-          class="fa-icon"
+          :class="['fa-icon', model.value ? 'icon-checkbox--checked' : '']"
           :color="model.value ? onColor : offColor"
           @click="model.value = !model.value"
         ></font-awesome-icon>
         <v-icon
           v-else
-          class="md-icon"
+          :class="['md-icon', model.value ? 'icon-checkbox--checked' : '']"
           :color="model.value ? onColor : offColor"
           @click="model.value = !model.value"
         >{{ model ? onIcon : offIcon }}
@@ -31,6 +36,7 @@ export interface IconCheckboxProps {
   offIcon: string;
   onColor?: string;
   offColor?: string;
+  label: string;
 }
 
 const modelValue = defineModel();
@@ -38,4 +44,23 @@ const props = withDefaults(defineProps<IconCheckboxProps>(), {
   onColor: "white",
   offColor: "gray",
 });
+
+function update() {
+  if (typeof modelValue.value === "boolean") {
+    modelValue.value = !modelValue.value;
+  } else { // it's an array
+    const index = modelValue.value.indexOf(props.value);
+    if (index >= 0) {
+      modelValue.value = modelValue.value.slice(0, index).concat(modelValue.value.slice(index + 1));
+    } else {
+      modelValue.value = [...modelValue.value, props.value];
+    }
+  }
+}
 </script>
+
+<style scoped>
+.icon-checkbox--checked {
+  filter: drop-shadow(0 0 2px rgb(var(--v-theme-on-surface)));
+}
+</style>
